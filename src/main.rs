63cffcfecbc2cead92ca6817a6add7af2720e6e5
N4xv <andresbperez7@gmail.com>
2026-05-22 14:58:16 +0200

    Implementacion en el main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, world!");
-}
+use clap::Parser;
+use tokio::net::{TcpListener, TcpStream};
+use tracing::{info, error, Level};
+use tracing_subscriber::FmtSubscriber;
+use std::error::Error;
